753dcb76db3bdf677cb7272b3822154b3769c44e
Mack Ramirez <mnrdev510@gmail.com>
2024-02-03 08:10:01 -0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// All our components here
+import AllBoards from './Components/allBoards'
+
+function App() {
+  
+
+  return (
+    <>
+      <AllBoards/>
+      Hello world
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/allBoards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AllBoards = () => {
+  return (
+      <nav>
+          <img src="../assets/image/logo-dark.svg" alt="logo" />
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+    </nav>
+  )
+}
+
+export default AllBoards;
